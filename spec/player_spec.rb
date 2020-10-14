04073a948ae9cfbd768ff51fb847f6a455e13784
“NathanielHopkins” <nathanielchopkins@gmail.com>
2020-10-14 15:51:59 -0700

    write expectations for #initialize in player_spec

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,6 +2,21 @@ require "player"
 
 describe "Player" do
 
+  describe "#initialize" do
+    let(:player) {Player.new(hand,pot)}
+    it "takes in a hand and a pot" do
+      expect{player}.not_to raise_error
+    end
 
+    it "sets and reads @hand as received hand" do
+      expect(player.hand).to eq(hand)
+    end
 
+    it "sets and reads @pot as received pot" do
+      expect(player.pot).to eq(pot)
+    end
+  end
+
+  describe "#discard" do
+  end
 end
