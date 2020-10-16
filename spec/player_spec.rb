cd1a3a70674fc60865f0480615d52141008a7178
“NathanielHopkins” <nathanielchopkins@gmail.com>
2020-10-16 12:55:18 -0700

    write tests for #discard_phase, #discard_input, and #get_action

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -69,34 +69,85 @@ describe "Player" do
       expect(player.cards).to eq([card3,card4,card5])
     end
   end
+  
+  describe "#discard_display" do
+    it "helper method that prints each card.to_s, provides user prompt" do
+      allow_any_instance_of(Player).to receive(:discard_display).and_return(nil)
+      expect{player.discard_display}.not_to raise_error
+    end
+  end
 
-  describe "#discard_prompt" do
+  describe "#discard_input" do
 
-    it "calls #discard to remove cards from player cards" do
-      allow(:discard_prompt).to receive(:gets).and_return("card1,card2")
-      expect(player.cards).to eq([card2,card3,card4,card5])
+    it "helper method that gets user input for discard and returns as an array" do
+      allow_any_instance_of(Player).to receive(:gets).and_return('1,2')
+      expect(player.discard_input).to be_an(Array)
+    end
+    
+    context "up to three cards are entered" do
+      it  "returns cards matching input from discard_display" do
+        allow_any_instance_of(Player).to receive(:gets).and_return('1,2')
+        expect(player.discard_input).to eq([card1,card2])
+      end
     end
 
-    it "returns an array" do
-      allow(:discard_prompt).to receive(:gets).and_return("card1,card2")
-      expect(player.discard_prompt).to be_an(Array)
+    context "more than three numbers or invalid input are entered" do
+      it "rescues and retries" do
+        allow_any_instance_of(Player).to receive(:gets).and_return('1,2,3,4','aljfa','1,2')
+        expect(player.discard_input).to eq([card1,card2])
+      end
     end
 
-    it "returns discarded cards" do
-      allow(:discard_prompt).to receive(:gets).and_return("card1,card2")
-      expect(player.discard_prompt).to eq([card1,card2])
+    context "'none' is entered" do
+      it "returns ['none']" do
+        allow_any_instance_of(Player).to receive(:gets).and_return('none')
+        expect(player.discard_input).to eq('none')
+      end
+    end
+  end
+
+  describe "#discard_phase" do
+
+    context "discard_input returns an array of cards" do
+      it "calls #discard to remove cards from @cards" do
+        allow_any_instance_of(Player).to receive(:discard_input).and_return([card1,card2])
+        allow_any_instance_of(Player).to receive(:discard_display).and_return(nil)
+        player.discard_phase
+        expect(player.cards).to eq([card3,card4,card5])
+      end
+
+      it "returns array of discarded cards" do
+        allow_any_instance_of(Player).to receive(:discard_input).and_return([card1,card2])
+        allow_any_instance_of(Player).to receive(:discard_display).and_return(nil)
+        expect(player.discard_phase).to eq([card1,card2])
+      end
+    end
+
+    context "discard_input returns 'none'" do
+      it "returns false" do
+        allow_any_instance_of(Player).to receive(:discard_input).and_return('none')
+        allow_any_instance_of(Player).to receive(:discard_display).and_return(nil)
+        expect(player.discard_phase).to eq(false)
+      end
+
+      it "does not change @cards" do
+        allow_any_instance_of(Player).to receive(:gets).and_return('none')
+        allow_any_instance_of(Player).to receive(:discard_display).and_return(nil)
+        player.discard_input
+        expect(player.cards).to eq([card1,card2,card3,card4,card5])
+      end
     end
   end
 
   describe "#get_action" do
-    it "returns :fold, :see, or :raise" do
-      allow(:get_action).to receive(:gets).and_return("fold")
-      expect(player.get_action).to be(:fold).or be(:see).or be(:raise)
+    it "gets user input and returns :fold, :see, or :raise" do
+      allow_any_instance_of(Player).to receive(:gets).and_return("fold")
+      expect(player.get_action).to eq(:fold)
     end
 
-    it "raises error, rescues, and retries if user input is not 'folds','see',or'raise'" do
-      allow(:get_action).to receive(:gets).and_return("bluff","raise")
-      expect{player.get_action}.not_to raise_error
+    it "raises error, rescues, and retries if user input is not 'fold','see',or'raise'" do
+      allow_any_instance_of(Player).to receive(:gets).and_return('bluff','beg','see')
+      expect(player.get_action).to eq(:see)
     end
   end
 end
